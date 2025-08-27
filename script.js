173cd2d90446cde48eaf5a13a660640e7072689b
harunagangx <mtruongggg@gmail.com>
2025-08-27 09:58:16 +0700

    update: navbar behavior and UI on scroll
    fix the sticky navbar behavior on scroll
    
    add shadow to navbar when scroll to improves visual separation between navbar and page content

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+document.addEventListener('scroll', function () {
+  const header = document.getElementById('header');
+
+  if (window.scrollY >= 120) {
+    header.classList.add('shadow-lg');
+  } else {
+    header.classList.remove('shadow-lg');
+  }
+});
+
 document.getElementById('newsletter-form').addEventListener('submit', function (event) {
   event.preventDefault();
   alert('Thank you for subscribing!');
